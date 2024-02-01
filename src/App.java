7f236f6e2aa9bc7302ceec8e573d584204ea9f36
darryl1975 <darryl1975@hotmail.com>
2024-02-01 16:34:17 +0800

    complete workshop 01

--- src/App.java
+++ src/App.java
@@ -1,5 +1,66 @@
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to your shopping cart");
+
+        List<String> cartItems = new ArrayList<>();
+
+        Console cons = System.console();
+        String input = "";
+        input = input.toLowerCase();
+
+        while (!input.equals("quit")) {
+
+            if (input.equals("list")) {
+
+                if (cartItems.size() > 0) {
+                    for (int i = 0; i < cartItems.size(); i++) {
+                        // System.out.println((i+1) + cartItems.get(i));
+                        System.out.printf("%d. %s\n", (i + 1), cartItems.get(i));
+                    }
+                } else {
+                    System.out.println("Your cart is empty");
+                }
+            }
+
+            if (input.startsWith("add")) {
+                input = input.replace(',', ' ');
+
+                Scanner scan = new Scanner(input.substring(4));
+                String stringVal = "";
+                while (scan.hasNext()) {
+                    stringVal = scan.next();
+                    cartItems.add(stringVal);
+                }
+            }
+
+            if (input.startsWith("delete")) {
+                Scanner scan = new Scanner(input.substring(6));
+                String stringVal = "";
+
+                while (scan.hasNext()) {
+                    stringVal = scan.next();
+
+                    Integer cartPosition = Integer.parseInt(stringVal);
+                    cartPosition = cartPosition - 1;
+
+                    if (cartPosition < cartItems.size()) {
+                        // delete the item from cart
+                        cartItems.remove(cartPosition);
+
+                    } else {
+                        System.err.println("Incorrect item index");
+                    }
+                }
+            }
+
+        }
+
+        System.out.println("Bye! Bye! See you again...");
+
     }
 }
